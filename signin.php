08d7a4ac76395f5b1f334cc67bafb3ed97afa03c
DESKTOP-IGVID2J\arkin <arking21344@gmail.com>
2021-02-02 23:09:01 +0500

    creating sign up and sign in page

--- signin.php
+++ signin.php
@@ -27,10 +27,34 @@
             </div>
         </div>
         <!-- page Header End -->
-
-
-
-
+         <!-- Login form -->
+         <div class="contact">
+            <div class="container">
+                <div class="section-header">
+                    <h2>Log In</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-6  text-center form_center">
+                        <div class="contact-form">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" class="form-control text-center" placeholder="Username" required="required" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control text-center" placeholder="Password" required="required" />
+                                </div>
+                                <div>
+                                    <button class="btn" type="submit">Log in</button>
+                                </div>
+                                <hr>
+                                <p class="mt-3 ">Not registered ? <span class="font-weight-bold font-italic"><a href="signup.php"> Create an account</a></span></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Login form End-->
 
         <?php include "layout/footer.php" ?>
 
